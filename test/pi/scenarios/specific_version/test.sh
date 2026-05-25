72e09f57a26bb11cc1a53bf1c3593763a5ad6471
Gustavo Bosmak <me@bsmk.xyz>
2026-05-25 13:38:06 -0300

    test: use devcontainer features test lib

--- test/pi/scenarios/specific_version/test.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-# Scenario test: specific_version
--- test/pi/specific_version.sh
+++ test/pi/specific_version.sh
@@ -5,8 +5,6 @@ source dev-container-features-test-lib
 
 check "pi binary is installed" bash -c "command -v pi"
 check "pi version command works" bash -c "pi --version"
-check "node dependency is installed" bash -c "command -v node"
-check "node version command works" bash -c "node --version"
 check "PI_CODING_AGENT_DIR is set" bash -c "test -n \"${PI_CODING_AGENT_DIR}\""
 check "PI_CODING_AGENT_DIR points to shared config mount" bash -c "test \"${PI_CODING_AGENT_DIR}\" = \"/usr/local/share/pi-agent-config\""
 
